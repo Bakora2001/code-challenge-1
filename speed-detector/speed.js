1049d43fe1e12c3d6b2a8931cd95cc512d165adf
Maxwel <maxwellbakora@gmail.com>
2024-03-24 22:42:09 +0300

    Added code

--- speed-detector/speed.js
+++ speed-detector/speed.js
@@ -1,4 +1,19 @@
-const speed = prompt('Enter speed of car');
-if ("speed < 70") {
-    
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const maxPoints = 12;
+
+    if (speed <= speedLimit) {
+        console.log("Ok");
+    } else {
+        let demeritPoints = Math.floor((speed - speedLimit) / 5);
+        if (demeritPoints >= maxPoints) {
+            console.log("License suspended");
+        } else {
+            console.log(`Points: ${demeritPoints}`);
+        }
+    }
 }
+// Testing the function
+checkSpeed(80); 
+checkSpeed(); 
+checkSpeed();
